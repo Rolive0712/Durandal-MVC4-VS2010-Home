74bec62ddf691086204707a7b274b72835476ed6
roshitgit <roshitrajan@gmail.com>
2014-06-22 21:20:47 +0530

    Create datacontext.js

---
+++ DurandalSPAMVC4/App/services/datacontext.js
@@ -0,0 +1,29 @@
+/// <reference path="../../Scripts/_references.js" />
+
+define(['durandal/system',
+        'services/logger'], function (system, logger) {
+
+            function getOrders() {
+
+                var param = { viewname: "northwind", orderid: 1 };
+                //return $.getJSON("/Northwind/GetOrders", { viewname: "northwind", orderid: 1 }, function (result) {
+                //});
+
+                return $.ajax({
+                    url: "/Northwind/GetOrders",
+                    type: "POST",
+                    cache: false,
+                    data: JSON.stringify(param),
+                    async: true,
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8"
+
+                });
+            }
+
+            var datacontext = {
+                getOrders: getOrders
+            }
+
+            return datacontext;
+        });
