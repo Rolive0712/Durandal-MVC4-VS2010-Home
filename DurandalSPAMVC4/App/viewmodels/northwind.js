9b9adc9bea143e202aa06ee739fdb0119ce324da
roshitgit <roshitrajan@gmail.com>
2014-07-06 16:27:55 +0530

    Update northwind.js

--- DurandalSPAMVC4/App/viewmodels/northwind.js
+++ DurandalSPAMVC4/App/viewmodels/northwind.js
@@ -1,5 +1,10 @@
+/// <reference path="../../Scripts/_references.js" />
+//define(['durandal/app', 'services/logger', 'services/datacontext'], function (app, logger, datacontext) {
 
-define(['durandal/app', 'services/logger', 'services/datacontext'], function (app, logger, datacontext) {
+define(function (require) {
+    var app = require('durandal/app'),
+        logger = require('services/logger'),
+        datacontext = require('services/datacontext');
 
     //An AJAX call is an asyncronous task so you need to return a promise in the activate function for 
     //tell to durandal to wait until the data is retrieved before to proceed to apply bindings.
@@ -11,34 +16,38 @@ define(['durandal/app', 'services/logger', 'services/datacontext'], function (ap
     vm.isAttachedToView = ko.observable(false);
 
     vm.activate = function () {
-        //debugger;
         var self = this;
         //self.deferred = $.Deferred();
         vm.isLoading(true);
-        $.when(datacontext.getOrders()).done(vm.successCallback).fail(vm.failCallback);
 
+        if (amplify.store("NorthwindOrders")) {
+            vm.successCallback(amplify.store("NorthwindOrders"));
+            toastr.info("Retrieved data from LocalStorage Cache");
+        }
+        else {
+            $.when(datacontext.getOrders()).done(vm.successCallback).fail(vm.failCallback);
+            toastr.info("Cache expired : Retrieved data from server");
+        }
         //return self.deferred.promise();
         return true;
-        
+
     };
 
-    vm.successCallback = function (result) {
-        //debugger;
-        //vm.orders = ko.toJSON(result);
-        vm.orders = result;
+    vm.successCallback = function (data) {
+        if (amplify.store("NorthwindOrders") === undefined)
+            amplify.store("NorthwindOrders", data, { expires: 60000 });
+        vm.orders = data;
         vm.isLoading(false);
         vm.isAttachedToView(true);
         //self.deferred.resolve();
     };
 
     vm.failCallback = function (jqXHR, textStatus, errorThrown) {
-        //debugger;
         console.log("Error: " + jqXHR.responseText);
-       // vm.deferred.reject();
+        // vm.deferred.reject();
     };
 
     vm.canDeactivate = function () {
-        //debugger;
         return true;
     };
 
