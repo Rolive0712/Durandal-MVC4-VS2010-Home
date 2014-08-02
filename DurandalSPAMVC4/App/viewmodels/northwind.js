885f7b73730e224b55ef8d84c928838b3c1d4fe0
roshitgit <roshitrajan@gmail.com>
2014-08-02 11:26:21 +0530

    Update northwind.js

--- DurandalSPAMVC4/App/viewmodels/northwind.js
+++ DurandalSPAMVC4/App/viewmodels/northwind.js
@@ -1,5 +1,4 @@
 /// <reference path="../../Scripts/_references.js" />
-//define(['durandal/app', 'services/logger', 'services/datacontext'], function (app, logger, datacontext) {
 
 define(function (require) {
     var app = require('durandal/app'),
@@ -8,6 +7,7 @@ define(function (require) {
 
     //An AJAX call is an asyncronous task so you need to return a promise in the activate function for 
     //tell to durandal to wait until the data is retrieved before to proceed to apply bindings.
+
     var vm = {};
 
     vm.orders = ko.observableArray([]);
@@ -18,25 +18,27 @@ define(function (require) {
     vm.activate = function () {
         var self = this;
         //self.deferred = $.Deferred();
+
         vm.isLoading(true);
 
-        if (amplify.store("NorthwindOrders")) {
-            vm.successCallback(amplify.store("NorthwindOrders"));
-            toastr.info("Retrieved data from LocalStorage Cache");
-        }
-        else {
-            $.when(datacontext.getOrders()).done(vm.successCallback).fail(vm.failCallback);
-            toastr.info("Cache expired : Retrieved data from server");
-        }
+        datacontext.getOrders({
+            success: vm.successCallback,
+            error: vm.failCallback
+        });
         //return self.deferred.promise();
         return true;
-
     };
 
     vm.successCallback = function (data) {
-        if (amplify.store("NorthwindOrders") === undefined)
+        if (amplify.store("NorthwindOrders")) {
+            vm.orders = amplify.store("NorthwindOrders");
+            toastr.info("Retrieved data from LocalStorage Cache");
+        }
+        else {
             amplify.store("NorthwindOrders", data, { expires: 60000 }); // cache data in local storage for 1 min (60000 ms)
-        vm.orders = data;
+            vm.orders = data;
+            toastr.info("Cache expired : Retrieved data from server");
+        }
         vm.isLoading(false);
         vm.isAttachedToView(true);
         //self.deferred.resolve();
@@ -51,13 +53,13 @@ define(function (require) {
         return true;
     };
 
-    /*vm.viewAttached = function () {
-        debugger;
+    vm.viewAttached = function () {
+        //debugger;
         app.showMessage('View Attached', 'Success', ['OK']);
-        //log('View Attached', null, true);
+        log('View Attached', null, true);
         vm.isAttachedToView(true);
         return true;
-    };*/
+    };
 
     return vm;
 });
