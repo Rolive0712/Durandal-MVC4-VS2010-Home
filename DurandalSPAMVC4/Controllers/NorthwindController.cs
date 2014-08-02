db7e3d6dd864c61082695bc265b4696c0342f096
roshitgit <roshitrajan@gmail.com>
2014-08-02 11:33:34 +0530

    Update NorthwindController.cs

--- DurandalSPAMVC4/Controllers/NorthwindController.cs
+++ DurandalSPAMVC4/Controllers/NorthwindController.cs
@@ -1,26 +1,22 @@
-using AttributeRouting.Web.Mvc;
+using DapperORMDataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Http;
-using DapperORMDataAccess;
 
-namespace DurandalSPAMVC4.Controllers
+namespace MVCDurandalWeyland.Controllers
 {
     public class NorthwindController : Controller
     {
         private readonly INorthWindRepository iNorthRep = new NorthwindRepository();
 
-        [GET("/App/Views/{viewname}.html")]
-        public ActionResult Index(string viewname)
+        public ActionResult Index()
         {
-            string demo = "Here could be your advertisement!";
-            return View("~/Views/Durandal/" + viewname + ".cshtml", model: demo);
+            string demo = "Here could be your Weyland!";
+            return View(model: demo);
         }
 
-        //[GET("/App/Views/{viewname}.html")]
         public JsonResult GetOrders(string viewname, int orderid)
         {
             try
@@ -30,25 +26,6 @@ namespace DurandalSPAMVC4.Controllers
             catch (Exception ex)
             {
                 return Json(ex.Message);
-            }
-        }
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
             }
         }
 
